f170a4c383828b8e1e50adfa3b1ff35a0b09ea72
Laura <preston.laura92@gmail.com>
2024-09-10 12:39:22 +0200

    Ajout fontawesome

--- portfolio/src/Components/Introduction/Introduction.jsx
+++ portfolio/src/Components/Introduction/Introduction.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import "./Introduction.scss";
 import background_intro from "../../Assets/background_intro.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowsDownToLine } from "@fortawesome/free-solid-svg-icons";
 
 const Introduction = () => {
   return (
     <section className="Introduction">
       <img className="Introduction__background" src={background_intro} alt="background" />
-      <h2 className="Introduction__title">À propos</h2>
-      <p className="Introduction__text">LLorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget felis eget nunc lacinia</p>
+      <h2 className="Introduction__title">Développeuse web junior</h2>
+      <p className="Introduction__text">Hey, je m'appelle Laura Preston. Je suis une développeuse web frontend en reconversion professionnelle.</p>
+      <FontAwesomeIcon className="Introduction__arrowsdown" icon={faArrowsDownToLine} />
     </section>
   );
 };
